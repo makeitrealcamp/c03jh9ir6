59206c3a843ba1dfc55966b20ff94b172d21e217
jhon11390 <meljhon2@gmail.com>
2020-03-04 21:57:14 -0500

    se añade funcion de crear actores y formulario

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,5 +1,15 @@
 class ActorsController < ApplicationController
     def index
-        @actor = Actor.all
+        @actors = Actor.all
+    end
+
+    def new
+        @actor = Actor.new
+    end
+
+    def create
+         @actor = Actor.new(name: params[:actor][:name], bio: params[:actor][:bio], birth_date: params[:actor][:birth_date], birth_place: params[:actor][:birth_place], image_url: params[:actor][:image_url], alive: params[:actor][:alive], death_date: params[:actor][:death_date], death_place: params[:actor][:death_place])
+         @actor.save
+         redirect_to :controller => "actors", :action => "index"
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :actors
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'actors', to: 'actors#index'
+  #get 'actors', to: 'actors#index'
 end
