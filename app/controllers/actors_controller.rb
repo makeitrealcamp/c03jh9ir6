04283608b0c2103d71af1972aac9598205a6845e
dacardenasa <diego.cardenasaleg@gmail.com>
2020-03-18 10:11:01 -0500

    Add actors create form to the project, add views, routes and actions in Actors controller

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,6 +1,42 @@
 class ActorsController < ApplicationController
+
   def index
     @actores = Actor.all
     render 'layouts/index'
+  end
+
+  def new
+    @actor = Actor.new
+    render 'layouts/new'
+  end
+
+  def create
+    @alive = ActiveModel::Type::Boolean.new.cast(params[:alive])
+    if @alive == true
+      @actor = Actor.new(name: "#{params[:name]}", 
+          bio: "#{params[:bio]}", 
+          birth_date: "#{params[:birthdate]}",
+          birth_place: "#{params[:birthplace]}",
+          image_url: "#{params[:image_url]}",
+          alive: @alive)
+      else
+      @actor = Actor.new(name: "#{params[:name]}", 
+          bio: "#{params[:bio]}", 
+          birth_date: "#{params[:birthdate]}",
+          birth_place: "#{params[:birthplace]}",
+          image_url: "#{params[:image_url]}",
+          alive: @alive,
+          death_date: "#{params[:deathdate]}",
+          death_place: "#{params[:deathplace]}")      
+    end
+
+    if @actor.save
+      @actores = Actor.all
+      render 'layouts/index'
+    else
+      @errors = @actor.errors.full_messages
+      render 'layouts/new'
+    end
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  get 'actors', to: 'actors#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'actors', to: 'actors#index'
+
+  post 'actors', to: 'actors#create'
+  
+  get 'actors/new', to: 'actors#new'
+
 end
