68fb26736d878eaa9e57a3bf5e70da88da24bb16
Daniel Londoño <daniel_eslonh@outlook.com>
2020-10-22 13:13:42 -0500

    create view index

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,2 +1,10 @@
 class ActorsController < ApplicationController
+	def index				# método que se utiliza para listar los actores según el archivo routes.rb
+		@actors = Actor.all # se utiliza el nombre del model es decir Actor, y se guarda todo el registro en la variable @actors
+	end
+
+	def new
+		@actors = Actor.new #método que se utiliza para crear un nuevo actor según archivo routes.rb
+	end
+
 end
