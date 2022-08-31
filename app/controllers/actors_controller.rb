3f00a2db41f0761c7d0208161c31fa5f08fdc293
CristianRojasBetancur <cristianrojasbetancur@gmail.com>
2022-08-31 18:50:42 -0500

    first commit

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,2 +1,10 @@
 class ActorsController < ApplicationController
+  def index
+    @actors = Actor.all
+    p @actors[0].image_url
+  end
+
+  def show
+    @actor = Actor.find(params[:id])
+  end
 end
