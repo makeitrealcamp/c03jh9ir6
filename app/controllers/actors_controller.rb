0d9b1b30e3f06579ad5971c76d13b4ef90957a92
CristianRojasBetancur <cristianrojasbetancur@gmail.com>
2022-08-31 21:46:55 -0500

    form new actor and create action

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -7,4 +7,17 @@ class ActorsController < ApplicationController
   def show
     @actor = Actor.find(params[:id])
   end
+
+  def new
+    @actor = Actor.new
+  end
+
+  def create
+    @actor = Actor.create(actor_params)
+    redirect_to actors_path
+  end
+
+  def actor_params
+    params.require(:actor).permit(:name).permit(:bio).permit(:birth_date).permit(:birth_place).permit(:image_url).permit(:alive).permit(:death_date).permit( :death_place)
+  end
 end
