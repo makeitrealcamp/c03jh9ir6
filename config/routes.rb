835d46c3096c59c4d4dbbc8dc99b1d95f09382db
CaroDGomez <caro5952@gmail.com>
2022-09-08 21:28:38 -0500

    Make it Real: Creando actores

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/actors', to: 'actors#actors'
+  get '/actors', to: 'actors#index'
+  get '/actors/new', to: 'actors#new', as: 'new_actor'
+  post '/actors', to: 'actors#create'
 end
